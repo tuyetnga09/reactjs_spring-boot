242ee96ef1f3e5583adeef644abb680f3ee8a9ab
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-07-21 23:32:40 +0700

    add

--- react-fontend/src/App.js
+++ react-fontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter  as Router, Route, Switch } from "react-router-dom";
 import Employees from "./components/ListEmployee";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CreateEmployee from "./components/CreateEmployee";
 
 function App() {
   return (
@@ -13,7 +14,10 @@ function App() {
                 <div className="container">
                     <Switch> 
                           <Route path="/" exact component={Employees} />
-                          <Route path="/employees" component={Employees} />                      
+                          <Route path="/employees" component={Employees} />    
+                          <Route path="/add-employee" component={CreateEmployee} />   
+                           
+
                     </Switch>
                 </div>
               <Footer />
--- react-fontend/src/components/ListEmployee.jsx
+++ react-fontend/src/components/ListEmployee.jsx
@@ -1,4 +1,4 @@
-//import React và Component từ module "react" được sử dụng để tạo 
+//import React và Component từ module "react" được sử dụng để tạo
 //và quản lý các thành phần trong ứng dụng React
 import React, { Component } from "react";
 import EmployeeService from "../service/EmployeeService";
@@ -6,23 +6,35 @@ class Employees extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //khởi tạo mảng trống 
+      //khởi tạo mảng trống
       employess: [],
     };
+
+    //sử dụng để ràng buộc (bind) phương thức addEmployee() với đối tượng (instance) của lớp Employees.
+    this.addEmployee = this.addEmployee.bind(this);
   }
-  //gọi API thông qua EmployeeService.getEmployees() 
+  //gọi API thông qua EmployeeService.getEmployees()
   //Khi yêu cầu trả về dữ liệu từ API, chúng ta sử dụng phương thức then() để xử lý kết quả trả về
-  
-  componentDidMount(){
-    EmployeeService.getEmployees().then((responese) =>{
-      this.setState({employess: responese.data})
+
+  componentDidMount() {
+    EmployeeService.getEmployees().then((responese) => {
+      this.setState({ employess: responese.data });
     });
-    
+  }
+
+  // Đoạn mã này sử dụng thư viện react-router-dom để chuyển hướng người dùng đến một đường dẫn khác trong ứng dụng.
+  addEmployee() {
+    this.props.history.push("/add-employee");
   }
   render() {
     return (
       <div>
         <h2 className="text-center">List Employee</h2>
+
+        <button class="btn btn-success"onClick={this.addEmployee}>
+          Add Employee
+        </button>
+
         <div className="row">
           <table class="table table-striped table-bordered">
             <thead>
@@ -36,18 +48,15 @@ class Employees extends Component {
               </tr>
             </thead>
             <tbody>
-                {
-                    this.state.employess.map((
-                        employee, index )=> 
-                        <tr key={employee.id}>
-                           <td>{index + 1}</td>
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.emailId}</td>
-                        </tr>
-                    )
-                }
+              {this.state.employess.map((employee, index) => (
+                <tr key={employee.id}>
+                  <td>{index + 1}</td>
+                  <td>{employee.id}</td>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.emailId}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
